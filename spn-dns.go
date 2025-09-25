a33db68f200163d90cb75f4acdb7459ee23a81d4
Oxmix <oxmix@me.com>
2025-09-25 04:05:14 +0300

    v1.9.2

---
+++ spn-dns.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+const dnsConfFile = "/tmp/netip-spn-dns"
+
+var preDnsHosts = ""
+
+type SpnDnsBundle struct {
+	WgId    int          `json:"wgId"`
+	Forward []string     `json:"forward"`
+	Rules   []SpnDnsRule `json:"rules"`
+}
+
+type SpnDnsRule struct {
+	IP   string `json:"ip"`
+	Host string `json:"host"`
+}
+
+type SpnDns struct{}
+
+func NewSpnDns() *SpnDns {
+	return &SpnDns{}
+}
+
+func (d *SpnDns) shell(command string, errIgnore bool) string {
+	out, err := exec.Command("sh", "-c", command).CombinedOutput()
+	if err != nil && !errIgnore {
+		log.Println("dns shell err:", err, "command:", command)
+	}
+	return strings.TrimSpace(string(out))
+}
+
+func (d *SpnDns) up() {
+	go func() {
+		log.Println("spn dns up")
+		d.shell("/coredns -conf "+dnsConfFile+" > /var/log/spn-dns.log 2>&1", false)
+	}()
+}
+
+func (d *SpnDns) down() {
+	log.Println("spn dns down")
+	d.shell("pkill coredns", true)
+	err := os.Remove(dnsConfFile)
+	if err != nil && !os.IsNotExist(err) {
+		log.Println("spn dns clean conf err:", err)
+	}
+}
+
+func (d *SpnDns) Refresh(bundle *SpnDnsBundle) {
+	if len(bundle.Forward) == 0 || len(bundle.Rules) == 0 {
+		d.down()
+		return
+	}
+
+	forward := strings.Join(bundle.Forward, " ")
+	hosts := ""
+	for _, e := range bundle.Rules {
+		hosts += fmt.Sprintf("\n\t\t%s %s", e.IP, e.Host)
+	}
+
+	// if the rules have not changed
+	if preDnsHosts == hosts {
+		return
+	}
+	d.down()
+	preDnsHosts = hosts
+
+	conf := fmt.Sprintf(`.:53 {
+	bind netip-wg%d
+	hosts {%s
+		fallthrough
+	}
+	forward . %s
+	cache 60
+	#log
+	errors
+}`, bundle.WgId, hosts, forward)
+
+	err := os.WriteFile(dnsConfFile, []byte(conf), 0644)
+	if err != nil {
+		log.Println("dns save conf, err:", err)
+		return
+	}
+	d.up()
+}
+
+func (d *SpnDns) Destroy() {
+	d.down()
+}
